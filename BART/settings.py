d8d309295ae9df3d094c87e1e30e9d511ef55b95
NaolGB <naolgbasaye@gmail.com>
2022-11-13 19:47:54 -0500

    updated debug and allowed hosts in setting

--- BART/settings.py
+++ BART/settings.py
@@ -24,11 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = sec.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [
-
-]
+ALLOWED_HOSTS = ['bynaol.com', 'www.bynaol.com', 'bart.bynaol.com', 'www.bart.bynaol.com', '127.0.0.1']
 
 
 # Application definition
